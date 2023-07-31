3a16e8a8e5f841aacb5ab0bc5d6772617ec48dcc
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-07-31 15:06:51 +0700

    shuffle question

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,45 @@ import { Choice } from './question';
 export class AppComponent {
     questions = quizData;
     currentQuestionIndex = 0;
+    score = 0;
+
     audio = new Audio();
+
+    isEnd = false;
 
     constructor(){
       this.audio.src = './assets/audio/mixkit-retro-game-notification-212.wav'
+      this.newQuiz();
     }
 
     onClickChoice(choice: Choice){
       console.log(`user click: ${choice.text}`);
 
       this.playSound();
+      if(choice.isAnswer) this.score++;
       
       if(this.currentQuestionIndex < this.questions.length - 1){
         this.currentQuestionIndex++;
+      } else{
+        this.isEnd = true;
       }
     }
 
-    playSound(){
+    private playSound(){
       this.audio.load();
-      this.audio.play();
+      this.audio.addEventListener('canplaythrough',() => {
+        this.audio.play()
+      });
+    }
+
+    onClickNewQuiz(){
+      this.newQuiz();
+    }
+
+    private newQuiz(){
+      this.questions.sort((a,b) => 0.5 - Math.random());
+      this.isEnd = false;
+      this.currentQuestionIndex = 0;
+      this.score = 0;
     }
 }
