fe0080f72c5de9ffdf1a84fb6afe67a7f4e3f2ea
Joonisnotmonth <yada.2546.kks@gmail.com>
2023-07-31 13:52:53 +0700

    update playsound code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { quizData } from './data/quiz_data';
+import { Choice } from './question';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    question = {
-      text: 'Who painted the MonaLisa',
-      choices: ['Vincent van Gogh', 'Pablo Picasso', 'Michelangelo', 'Leonardo Da Vinci']
-    };
+    questions = quizData;
+    currentQuestionIndex = 0;
+    audio = new Audio();
 
-    onClickChoice(text: string){
-      console.log(text);
+    constructor(){
+      this.audio.src = './assets/audio/mixkit-retro-game-notification-212.wav'
+    }
+
+    onClickChoice(choice: Choice){
+      console.log(`user click: ${choice.text}`);
+
+      this.playSound();
+      
+      if(this.currentQuestionIndex < this.questions.length - 1){
+        this.currentQuestionIndex++;
+      }
+    }
+
+    playSound(){
+      this.audio.load();
+      this.audio.play();
     }
 }
